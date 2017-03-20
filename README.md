f2629a84504495b80c081a3b8c6dd28847aa4ad8
loatheb <zz.innj@gmail.com>
2017-03-21 00:12:54 +0800

    feat(init project): add handleRes function in index.js && fix for multiple load type

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# fetch-helper
-auto handle response after fetch
+# Fetch-Helper
+> auto handle response after fetch
+
+### Usage
+
+```
+import handleRes from '/path/to/module'
+
+import {handleRes} from '/path/to/module'
+```
+
+or
+
+```
+var handleRes = require('/path/to/module')
+```
+
+```
+fetch('/path/to/data')
+  .then(handleRes)
+  .then(res => {
+    // data here.
+  })
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const handleRes = response => {
+  const contentType = response.headers.get('Content-Type')
+
+  const isJson = /^application\/json/
+  const isForm = /([-\/])form-[\w\W]./
+  const isBuffer = /^(audio|vedio)[\w\W]./
+  const isText = /^text\/(html|xml|js|css)/
+  const isImg = /^image\/[\w\W]./
+
+  let result = null
+
+  if (isJson.test(contentType)) {
+
+    result = response.json()
+
+  } else if (isText.test(contentType)) {
+
+    result = response.text()
+
+  } else if (isForm.test(contentType)) {
+
+    result = response.formData()
+
+  }  else if (isBuffer.test(contentType)) {
+
+    result = response.arrayBuffer()
+
+  } else if (isImg.test(contentType)) {
+
+    result = response.blob()
+
+  }
+  return Promise.resolve(result, response)
+}
+
+module.exports = handleRes.handleRes = handleRes['default'] = handleRes
